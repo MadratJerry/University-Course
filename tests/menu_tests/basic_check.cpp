d95301f1983bc383627980507038c6472c75be10
Yotau Hong <crazymousethief@outlook.com>
2017-06-10 17:20:25 +0800

    Add googletest

---
+++ tests/menu_tests/basic_check.cpp
@@ -0,0 +1,22 @@
+/**
+ * @Author: crazymousethief
+ * @Date:   6/10/17
+ */
+
+#include "gtest/gtest.h"
+#include "../../lib/Menu.h"
+
+TEST(basic_check, test_basic_format) {
+    ParentMenu main{new LeafMenu{"wow"}};
+    testing::internal::CaptureStdout();
+    std::stringstream ss;
+    std::cin.rdbuf(ss.rdbuf());
+    ss << "0" << std::endl;
+    main.run();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "---------\n"
+            "|1. wow |\n"
+            "|0. Quit|\n"
+            "---------\n\n"
+            "Please select a choice: ");
+}
