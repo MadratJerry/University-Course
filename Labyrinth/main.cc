a508631c54be11524159edaab42e29b750cf8e70
Yotau Hong <crazymousethief@outlook.com>
2017-06-16 00:42:34 +0800

    Add Labyrinth

---
+++ Labyrinth/main.cc
@@ -0,0 +1,14 @@
+/**
+ * @Author: crazymousethief
+ * @Date:   6/16/17
+ */
+
+#include <iostream>
+#include "Maze.h"
+
+using namespace std;
+
+int main() {
+
+  return 0;
+}
---
+++ tests/labyrinth_tests/basic_check.cc
@@ -0,0 +1,23 @@
+/**
+ * @Author: crazymousethief
+ * @Date:   6/14/17
+ */
+
+#include "gtest/gtest.h"
+#include "Maze.h"
+
+using  namespace std;
+
+string maze1 = "0000000000\n"
+    "1010110101\n"
+    "0011100111\n"
+    "0100001000\n"
+    "0000000000\n"
+    "0000000000\n"
+    "1010110101\n"
+    "0011100111\n"
+    "0100001000\n"
+    "0000000000\n";
+
+TEST(labyrinth_check, test_load) {
+}
