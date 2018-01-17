53989d43528dfa8073a04aff77c1844952abd8f4
Yotau Hong <crazymousethief@outlook.com>
2018-01-17 23:31:17 +0800

    ➕ Add gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title:
+      'Curriculum Design of Introduction to The Design and Analysis of Algorithms',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
