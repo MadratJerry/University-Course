417ab479ada46c6e8eed58843e19e971c7f8fa39
Yotau Hong <crazymousethief@outlook.com>
2018-05-13 23:22:11 +0800

    ✨ Implement a new base model

---
+++ Course-Selection-System/src/models/College.java
@@ -0,0 +1,10 @@
+package models;
+
+import base.H;
+
+public class College extends H {
+    private String collegeId;
+    private String collegeName;
+    private String collegeHeadId;
+    private String collegeSummary;
+}
