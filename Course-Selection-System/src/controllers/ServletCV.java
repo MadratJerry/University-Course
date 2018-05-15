ae6e2a3b8d27d0fe34fffa6c66e047c773a26050
Yotau Hong <crazymousethief@outlook.com>
2018-05-15 21:23:27 +0800

    ✨ Done, finish it

--- Course-Selection-System/src/controllers/ServletCV.java
+++ Course-Selection-System/src/controllers/ServletCV.java
@@ -1,10 +1,44 @@
-@javax.servlet.annotation.WebServlet(name = "ServletCV")
-public class ServletCV extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+package controllers;
+
+import com.alibaba.fastjson.JSON;
+import models.CV;
+import utils.ServletJSON;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ServletCV", urlPatterns = "/cv/*")
+public class ServletCV extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getPathInfo().substring(1);
+        CV cv = ServletJSON.parse(request, CV.class);
+        if (!new CV().updateOneById(id, cv)) {
+            throw new IOException("Update error");
+        }
+    }
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getPathInfo().substring(1);
+        CV cv = new CV().findOneById(id);
+        if (cv == null) {
+            response.setStatus(404);
+        } else {
+            response.getWriter().println(JSON.toJSON(cv));
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getPathInfo().substring(1);
+        response.setStatus(new CV().deleteOneById(id) ? 200 : 404);
     }
 }
