b765ec3419e901ac03874c12d27d4db7e314d941
Yotau Hong <crazymousethief@outlook.com>
2019-02-28 21:46:40 +0800

    🔧 Add proxy config

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -1,13 +1,29 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+const {
+  override,
+  fixBabelImports,
+  addLessLoader,
+  overrideDevServer,
+} = require('customize-cra')
 
-module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-    style: true,
+module.exports = {
+  webpack: override(
+    fixBabelImports('import', {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true,
+    }),
+    addLessLoader({
+      javascriptEnabled: true,
+      modifyVars: { '@primary-color': '#25b864' },
+    }),
+  ),
+  devServer: overrideDevServer(config => {
+    config.proxy = {
+      '/api': {
+        target: 'http://localhost:8080',
+        pathRewrite: { '^/api': '' },
+      },
+    }
+    return config
   }),
-  addLessLoader({
-    javascriptEnabled: true,
-    modifyVars: { '@primary-color': '#25b864' },
-  }),
-)
+}
