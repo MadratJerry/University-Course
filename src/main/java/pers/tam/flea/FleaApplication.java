1e2a1fad6eb5caacb23c6e7c7dc4f0dd1379e3ee
Yotau Hong <crazymousethief@outlook.com>
2019-03-03 22:32:37 +0800

    🏗 Replace with Spring Boot

---
+++ src/main/java/pers/tam/flea/FleaApplication.java
@@ -0,0 +1,13 @@
+package pers.tam.flea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleaApplication.class, args);
+    }
+
+}
